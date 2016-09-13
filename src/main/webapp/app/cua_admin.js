51421de87a4be22b36db4d5e61dc6d30b2b86cfa
esteban_santiago <esteban_santiago@192.168.0.13>
2016-09-13 01:05:13 -0300

    Primer commit

--- src/main/webapp/app/cua_admin.js
+++ src/main/webapp/app/cua_admin.js
@@ -3,7 +3,18 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-use strict;
+'use strict';
 
-angular.module('cua_admin')
+var cua_admin = angular.module('cua_admin',['ngRoute']);
+
+cua_admin.config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/home', {
+            templateUrl: 'views/home.html'
+        }).when('/directory', {
+            templateUrl: 'views/directory.html'
+        }).otherwise({
+            redirect: '/home'
+        });
+}]);
+
 
