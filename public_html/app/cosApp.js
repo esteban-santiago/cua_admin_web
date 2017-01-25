43a0d709f54f480bbee523a21fe0a63be48115ee
esantiago <esantiago@192.168.77.75>
2017-01-25 20:12:33 -0300

    Commit Arranque de páginas

--- public_html/app/cosApp.js
+++ public_html/app/cosApp.js
@@ -1,16 +1,40 @@
 'use strict';
 
+
 var cosApp = angular.module('cosApp',['ngRoute']);
 
-cosApp.config(['$routeProvider', function($routeProvider) {
-        $routeProvider.when('/home', {
-            templateUrl: 'views/home.html'
-        }).when('/directory', {
-            templateUrl: 'views/directory.html'
-        }).otherwise({
-            redirect: '/home'
-        });
+cosApp.config(['$routeProvider',function($routeProvider) {
+        //alert('pepe');
+        //$routeProvider.hasPrefix('');
+        $routeProvider.when('/', { templateUrl: 'views/launch_pad/administration.html'})
+          .when('/accounting', { templateUrl: 'views/launch_pad/accounting.html'})
+          .when('/finance', { templateUrl: 'views/launch_pad/finance.html'})
+          .when('/operations', { templateUrl: 'views/launch_pad/operations.html'})
+          .when('/members', { templateUrl: 'views/spas/members.html'})
+          .otherwise({ redirect: '/' });
 }]);
 
-cosApp.controller()
 
+/*
+var cosApp = angular.module('cosApp', ['ngRoute']);
+
+cosApp.config(['$routeProvider',
+                function($routeProvider) {
+                  $routeProvider.
+                    when('/administration', {
+                      templateUrl: 'views/launch_pad/administration.html'
+                    }).
+                    when('/finance', {
+                      templateUrl: 'views/launch_pad/finance.html'
+                    }).
+                    when('/accounting', {
+                        templateUrl: 'views/launch_pad/accounting.html'
+                      }).
+                    otherwise({
+                      redirectTo: '/administration'
+                    });
+                }]);   
+
+cosApp.controller( 'cosLanCtrl', function ( $scope ) {
+});
+*/
