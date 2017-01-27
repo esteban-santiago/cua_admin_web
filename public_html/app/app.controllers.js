9d01cfe8ddb9fc89729263a3d3b925acce61346f
esteban_santiago <esteban_santiago@192.168.0.13>
2017-01-27 00:11:10 -0300

    Commit Angular

--- public_html/app/app.controllers.js
+++ public_html/app/app.controllers.js
@@ -1,7 +1,8 @@
 
 angular.module('app').controller('memberController', ['$scope','$http', function($scope, $http) {
     //$scope.myId = memberRepository.a();
-    $scope.get = function(id) {$http.get('https://jsonplaceholder.typicode.com:443/posts/'+id)
+    //$scope.get = function(id) {$http.get('https://jsonplaceholder.typicode.com:443/posts/'+id)
+    $scope.get = function(id) {$http.get('http://192.168.0.13:8080/sapi/user/'+id)
             .success(function(data) {
                 $scope.obj = data;
             }
