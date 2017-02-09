42d8e4f8f840097a63f0b0ceb95515c1116853e7
esantiago <esantiago@192.168.77.75>
2017-02-09 17:29:22 -0300

    Commit Date Range Picker

--- public_html/app/app.directives.js
+++ public_html/app/app.directives.js
@@ -15,8 +15,7 @@ angular.module('app').directive("datepicker", function () {
                 timePickerIncrement: 5,
                 locale: {
                     format: 'MM/DD/YYYY h:mm A'
-                }
-                ,
+                },
                 onSelect: function (dateText) {
                     updateModel(dateText);
                 }
@@ -24,20 +23,4 @@ angular.module('app').directive("datepicker", function () {
             $(elem).daterangepicker(options);
         }
     };
-})
-
-    .directive("datamask", function () {
-    return {
-        restrict: "A",
-        require: "ngModel",
-        link: function (scope, elem, attrs, ngModelCtrl) {
-            var updateModel = function (dateText) {
-                scope.$apply(function () {
-                    ngModelCtrl.$setViewValue(dateText);
-                });
-            };
-            console.log(elem);
-            $(elem).inputmask("dd/mm/yyyy", {"placeholder": "dd/mm/yyyy"});
-        }
-    };
-    });
+});
