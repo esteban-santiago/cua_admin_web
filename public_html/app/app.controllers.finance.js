c5a12a60a633d2f5e67bcf56152ee6baf011c5ae
esantiago <esantiago@192.168.77.75>
2017-03-09 18:00:31 -0300

    Páginas financieras

--- public_html/app/app.controllers.finance.js
+++ public_html/app/app.controllers.finance.js
@@ -1,7 +1,7 @@
 
 /* global moment */
 
-angular.module('app').controller('financeDocumentsController', ['$scope', 'financeDocumentsService', function ($scope, financeDocumentsService) {
+angular.module('app').controller('financeDocumentsController', ['$scope','financeDocumentsService', function ($scope, financeDocumentsService) {
         $scope.financeDocuments = financeDocumentsService.query();
         console.log($scope.financeDocuments);
 
